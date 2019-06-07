7e682a8273ef7a0133e421a5ec14ffaf8e4874e6
michaellinares <mjl761@nyu.edu>
2019-06-07 16:46:59 -0400

    Code is scannable with holes

--- MATLAB Code/FOGgenerator_.m
+++ MATLAB Code/FOGgenerator_.m
@@ -1,20 +1,8 @@
 function [ARRAY]=FOGgenerator_(ARRAY)
 
-%  ARRAY(:,:,1) = [ 1 0 1;
-%      0 1 0;
-%      1 0 1];
-% 
-%  ARRAY(:,:,2) = [ 0 1 0;
-%      1 0 1;
-%      0 1 0];
-%  ARRAY(:,:,3) = [ 1 0 1;
-%      0 1 0;
-%      1 0 1];
-% 
-
 [n0,m0,p0]= size(ARRAY);
-[n,m,p]= size(ARRAY);
-midpoint = ceil(n0/2);
+[~,m,p]= size(ARRAY);
+%midpoint = ceil(n0/2);
 factorr=4;
 offset = sqrt(2)*n0/2;
 n1=n0*factorr;
@@ -22,41 +10,40 @@ m1=m0*factorr;
 p1=p0*factorr;
 radius=n1/2;
 
-acceptableNrange = ceil(offset):floor(n1-offset);
+XfCentRange = ceil(offset):floor(n1-offset);
 
-nplacement = randsample(acceptableNrange,1);
+xfcent = randsample(XfCentRange,1);
+xfcent=404%8*ceil(xfcent/8)-4
 
-adjustment1 = sqrt((floor(n1-offset))^2-(nplacement-radius)^2);
+adjustment1 = sqrt((floor(n1-offset))^2-(xfcent-radius)^2);
 
-acceptableMrange = (ceil(adjustment1-radius)):(floor(adjustment1));
+YfCentRange = (ceil(adjustment1-radius)):(floor(adjustment1));
 
-mplacement = randsample(acceptableMrange,1);
+yfcent = randsample(YfCentRange,1);
+yfcent=404%8*ceil(yfcent/8)-4;
+adjustment2 = sqrt((floor(n1-offset))^2-(xfcent-radius)^2-(yfcent-radius)^2);
 
-adjustment2 = sqrt((floor(n1-offset))^2-(nplacement-radius)^2-(mplacement-radius)^2);
+ZfCentRange = (ceil(adjustment2-radius)):(floor(adjustment2));
 
-acceptablePrange = (ceil(adjustment2-radius)):(floor(adjustment2));
+zfcent = randsample(ZfCentRange,1);
+zfcent=404%8*ceil(zfcent/8)-4;
 
-pplacement = randsample(acceptablePrange,1);
+xfrightbuff = n1-xfcent-(ceil((n0-1)/2));
 
-
-
-
-nrightzerobarrier = n1-nplacement-(ceil((n0-1)/2));
-
-nleftzerobarrier = n1-n0-nrightzerobarrier;
+xfleftbuff = n1-n0-xfrightbuff;
 
 
-NZL=zeros(nleftzerobarrier,m,p);
+NZL=zeros(xfleftbuff,m,p);
 size(NZL);
-NZR=zeros(nrightzerobarrier,m,p);
+NZR=zeros(xfrightbuff,m,p);
 size(NZR);
 ARRAY=cat(1,NZL,ARRAY,NZR);
 
-[n,m,p]= size(ARRAY);
+[n,~,p]= size(ARRAY);
 
 
 
-mrightzerobarrier = m1-mplacement-(ceil((m0-1)/2));
+mrightzerobarrier = m1-yfcent-(ceil((m0-1)/2));
 
 mleftzerobarrier = m1-m0-mrightzerobarrier;
 
@@ -66,9 +53,9 @@ MZR=zeros(n,mrightzerobarrier,p);
 size(MZR);
 
 ARRAY=cat(2,MZL,ARRAY,MZR);
-[n,m,p]= size(ARRAY);
+[n,m,~]= size(ARRAY);
 
-prightzerobarrier = p1-pplacement-(ceil((m0-1)/2));
+prightzerobarrier = p1-zfcent-(ceil((m0-1)/2));
 
 pleftzerobarrier = p1-p0-prightzerobarrier;
 
@@ -80,35 +67,82 @@ size(ARRAY)
 
 %________________Generate fog
 
-count=1;
+
 density=0.5;
-allCol=8:8:800;
-col=2;
-while col<length(allCol)-1
-
-layerbound = [1 -2*n1/2 (allCol(col)-n1/2)^2];
-bounds=roots(layerbound);
-
-range = bounds(2):8:bounds(1);
-range=8*ceil(range/8);
-numberofextracubes = ceil(density*(bounds(1) - bounds(2))/8);%add standard dev and av
-
-for i=1:numberofextracubes
-    idx = ceil(length(range)*rand(1));
-    generatorposvec(i) = range(idx);
-     while sum(ismember(generatorposvec,generatorposvec(i)))>1
-         idx = ceil(length(range)*rand(1));
-         generatorposvec(i) = range(idx);
-     end
+allrow=8:8:800;
+fogrow=2;
+count=1;
+while fogrow<length(allrow)-1
+    rowvariable=false;
+    if sum(ismember(allrow(fogrow),(8*ceil((xfcent-99:8:xfcent+88)/8))))
+        rowvariable=true;
+    end
+    
+    layerbound = [1 -n1 (allrow(fogrow)-n1/2)^2];
+    bounds=roots(layerbound);
+    
+    range = min(bounds):8:max(bounds);
+    range=8*ceil(range/8);
+    numberofextracubes = abs(ceil(density*(max(bounds) - min(bounds)))/8); %add standard dev and av
+    exclusionzone=[];
+    if rowvariable
+        exclusionzone=8*ceil((yfcent-87:8:yfcent+88)/8);
+    end
+    for i=1:numberofextracubes
+        idx = ceil(length(range)*rand(1));
+        generatorposvec(i) = range(idx);
+        while sum(ismember(generatorposvec,generatorposvec(i)))>1
+            idx = ceil(length(range)*rand(1));
+            generatorposvec(i) = range(idx);
+        end
+    end
+    
+    
+    generatorposvec = sort(generatorposvec);
+    generatorposvec(1)=[];
+    generatorposvec(end)=[];
+    generatorposvec(ismember(generatorposvec,exclusionzone))=[];
+    
+    startcount=1;
+    endcount=0;
+    for j=1:numberofextracubes
+        
+        
+        if fix(j/2)~=j/2
+            selectedcell = generatorposvec(startcount);
+            startcount = startcount+1;
+        elseif fix(j/2)==j/2
+            selectedcell = generatorposvec(end-endcount);
+            endcount = endcount+1;
+        end
+        
+        %if sum(ARRAY(allrow(fogrow)-1,selectedcell-1,:))>1
+        %    variable=true;
+        %end
+        
+        
+        ZposRange = [1 -n1 ((selectedcell-n1/2)^2+(allrow(fogrow)-n1/2)^2)];
+        ZposBounds = 8*ceil(roots(ZposRange)/8);
+        
+        
+        ZposSlots = min(ZposBounds):8:max(ZposBounds);
+        ZposSlots(1) = [];
+        ZposSlots(end) = [];
+        zPos = ZposSlots(randi(length(ZposSlots)));
+        
+        
+        Zcoordinates = zPos:zPos+8 ; % these coordiantes include compatiable zPos and the cell length number of coordanites above it
+        Rowcoordinates = allrow(fogrow):allrow(fogrow+1) ; % these coordinates include the row positon and the cell length number ...
+        Colcoordinates = selectedcell:selectedcell+8 ;
+        ARRAY(Rowcoordinates,Colcoordinates,Zcoordinates) = 1; % the cube of coordiantes are "placed" inside the ARRAY
+        
+    end
+    startcount=1;
+    endcount=0;
+    generatorposvec=[];
+    fogrow=fogrow+1;
 end
-sort(generatorposvec)
-
-
-
-generatorposvec=0;
-col=col+10;
 end
 
 
-end
 
